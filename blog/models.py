8b4e5390320038220c9fc14cd594d2fdc7a5067e
shreyam1008 <shreyam1008@gmail.com>
2017-11-23 00:19:27 +0545

    created post list and post detail view

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
-    def get_queryset(selfself):
+    def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
